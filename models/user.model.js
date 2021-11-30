213f5de3f43e46f6fca611e26839ba7ac0255f45
himanshumishra508 <mishrahimanshu.hmi@gmail.com>
2021-11-30 09:58:40 +0530

    User model

---
+++ models/user.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
