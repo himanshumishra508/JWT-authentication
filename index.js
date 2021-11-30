c062ace23456caa53782c22d65d62aff57be1e2b
himanshumishra508 <mishrahimanshu.hmi@gmail.com>
2021-11-30 11:55:11 +0530

    DB connection established

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config();
+
+mongoose.connect(process.env.DB_CONNECTION_URI, () =>
+  console.log("Connected to DB")
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
